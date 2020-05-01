585d12910eabc6f3e5c74b8177cada17b68cec8d
wutong8023 <14964542+wutong8023@users.noreply.github.com>
2020-05-01 14:29:33 +1000

    framework outline

--- test/test_config.py
+++ test/test_config.py
@@ -1,6 +1,7 @@
 import unittest
 import config
-
+import sys
+import os
 
 class TestConfig(unittest.TestCase):
     def test_getdata(self):
@@ -14,4 +15,5 @@ class TestConfig(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    sys.path.append(os.path.dirname(sys.path[0]))
     unittest.main()
