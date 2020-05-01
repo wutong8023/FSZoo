e21e327348add6d7f5e242061faead6aed033198
wutong8023 <14964542+wutong8023@users.noreply.github.com>
2020-05-01 15:53:08 +1000

    test path

--- test/test_config.py
+++ test/test_config.py
@@ -2,6 +2,10 @@ import unittest
 import config
 import sys
 import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
 
 class TestConfig(unittest.TestCase):
     def test_getdata(self):
@@ -11,7 +15,6 @@ class TestConfig(unittest.TestCase):
             "/Users/parasol_tree/Resource/019 - Github/FSZoo/datasets/data_filtered/supervisedRE/semeval",
             dataset
         )
-
 
 
 if __name__ == '__main__':
